2f170f776e813da6d3be86cffa5d9608c35373b1
ArmCool <157279027+ArmCool@users.noreply.github.com>
2024-01-21 23:10:29 +0700

    Update generators.js

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,8 +1,8 @@
-Blockly.Python['aht20_read'] = function (block) {
-  Blockly.Python.definitions_['import_AHT20'] = 'import AHT20';
+Blockly.Python['aht10_read'] = function (block) {
+  Blockly.Python.definitions_['import_AHT10'] = 'import AHT10';
 
   var dropdown_type = block.getFieldValue('type');
 
-  var code = `AHT20.read()[${dropdown_type}]`;
+  var code = `AHT10.read()[${dropdown_type}]`;
   return [code, Blockly.Python.ORDER_NONE];
 };
